312e40285603e31de2a75a8a076c6b06ff6a7ab1
Michael Schwartz <mykes@sportstwo.com>
2014-07-04 15:26:41 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created with WebStorm.
+ * User: mschwartz
+ * Date: 9/28/13
+ * Time: 1:26 PM
+ */
+
+/*global module, require */
+
+module.exports = {
+    Image: require('lib/Image'),
+    Gif: require('lib/Gif')
+};
